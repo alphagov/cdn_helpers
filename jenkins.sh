1b0a770a105db0c5ba128de4cb4497ab7500d841
Matt Patterson <matt@reprocessed.org>
2011-04-06 17:17:59 +0100

    Add Jenkins build script

---
+++ jenkins.sh
@@ -0,0 +1,14 @@
+#!/bin/bash -x
+
+# install requirements if requirements.txt is newer than the virtualenv (assuming virtualenv has something in)
+if [ ! -d "/home/jenkins/virtualenvs/${JOB_NAME}" ] || [ "${WORKSPACE}/requirements.txt" -nt "/home/jenkins/virtualenvs/${JOB_NAME}" ]; then
+  mkdir -p /home/jenkins/virtualenvs
+  mkdir -p /home/jenkins/pip-cache
+  rm -rf "/home/jenkins/virtualenvs/${JOB_NAME}"
+  virtualenv "/home/jenkins/virtualenvs/${JOB_NAME}"
+  pip install --download-cache=/home/jenkins/pip-cache -E "/home/jenkins/virtualenvs/${JOB_NAME}" -r "${WORKSPACE}/requirements.txt"
+fi
+
+source "/home/jenkins/virtualenvs/${JOB_NAME}/bin/activate"
+
+nosetests -w py/test
